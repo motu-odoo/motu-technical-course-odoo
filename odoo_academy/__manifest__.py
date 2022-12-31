f823b0c64660e9b36f6703bf9bb9d234c05b8e09
motu-odoo <motu@odoo.com>
2022-12-31 01:40:44 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """"
+        Academy module to manage Training:
+        - Courses
+        - Sessions
+        -Attendees
+    """,
+    
+    'author': 'MOTU',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ], 
+    
+    'demo': [
+        
+    ],
+    
+    
+}
